1b09a22f1ef99c85de606fff3d331f312c4bd34d
juanrabaro <jrbaro@iesrafaelalberti.es>
2023-02-22 10:45:00 +0100

    acordeon no funciona

--- dist/index.a743020d.js
+++ dist/index.a743020d.js
@@ -1,2 +1,14 @@
+"use strict";
+const bloque = document.querySelectorAll(".acordeon__bloque");
+const h3 = document.querySelectorAll(".bloque__h3");
+console.log("hello world!");
+h3.forEach((cadaH3, i)=>{
+    h3[i].addEventListener("click", ()=>{
+        bloque.forEach((cadaBloque, i)=>{
+            bloque[i].classList.remove("activo");
+        });
+        bloque[i].classList.add("activo");
+    });
+});
 
 //# sourceMappingURL=index.a743020d.js.map
--- src/assets/js/acordeon.js
+++ src/assets/js/acordeon.js
@@ -1,4 +1,17 @@
+'use strict';
 
 
+const bloque = document.querySelectorAll('.acordeon__bloque');
+const h3 = document.querySelectorAll('.bloque__h3');
+console.log('hello world!');
 
+h3.forEach( (cadaH3, i) => {
+    h3[i].addEventListener('click', () => {
 
+        bloque.forEach( (cadaBloque, i) => {
+            bloque[i].classList.remove('activo');
+        })
+        bloque[i].classList.add('activo');
+        
+    })
+})
